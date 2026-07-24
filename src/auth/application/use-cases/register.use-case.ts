9c5967e0306adccc01428c8c6b080dc504b4b186
Cesarmc-0 <montealegrecesar45@gmail.com>
2026-07-24 03:38:06 -0500

    test(auth): tests del LoginUseCase + arreglar paths absolutos del dev

--- src/auth/application/use-cases/register.use-case.ts
+++ src/auth/application/use-cases/register.use-case.ts
@@ -1,26 +1,27 @@
-import { Injectable, ConflictException } from "@nestjs/common";
+import { Injectable, ConflictException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { AuthRepository } from "src/auth/domain/repositories/auth.repository.interface";
-import { RegisterDto } from "src/auth/presentation/dto/register.dto";
+import { AuthRepository } from '../../domain/repositories/auth.repository.interface';
+import { RegisterDto } from '../../presentation/dto/register.dto';
 
 @Injectable()
-export class RegisterUseCase{
-    constructor(private authRepository:AuthRepository){}
+export class RegisterUseCase {
+  constructor(private authRepository: AuthRepository) {}
 
-    async execute(dto:RegisterDto){
-        const existing = await this.authRepository.findByEmail(dto.correo);
-        if (existing) throw new ConflictException('El correo ya se encuentra registrado');
+  async execute(dto: RegisterDto) {
+    const existing = await this.authRepository.findByEmail(dto.correo);
+    if (existing)
+      throw new ConflictException('El correo ya se encuentra registrado');
 
-        const password_hash = await bcrypt.hash(dto.password,10);
+    const password_hash = await bcrypt.hash(dto.password, 10);
 
-        return this.authRepository.create({
-            correo: dto.correo,
-            nombre: dto.nombre,
-            apellido: dto.apellido,
-            numero_documento: dto.numero_documento,
-            tipo_documento_id: Number(dto.tipo_documento_id),
-            password_hash,
-            rol_id: 2,
-        });
-    }
+    return this.authRepository.create({
+      correo: dto.correo,
+      nombre: dto.nombre,
+      apellido: dto.apellido,
+      numero_documento: dto.numero_documento,
+      tipo_documento_id: Number(dto.tipo_documento_id),
+      password_hash,
+      rol_id: 2,
+    });
+  }
 }
